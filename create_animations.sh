c9e52d10d37b6c2575396d3b30ebb1ebc16979eb
jinyoonok2 <jinyoonok2@users.noreply.github.com>
2025-12-07 22:32:19 -0500

    Reduce GIF size: 5fps, every 2nd frame, 960px width

--- create_animations.sh
+++ create_animations.sh
@@ -13,26 +13,26 @@ echo -e "\n[Garden] Creating animations..."
 mkdir -p outputs/garden_brownplant_removal/animations
 
 echo "  - 07 merged..."
-ffmpeg -y -framerate 10 -pattern_type glob -i 'outputs/garden_brownplant_removal/07_final_visualization/merged/*.png' -vf scale=1920:-1 outputs/garden_brownplant_removal/animations/07_merged.gif
+ffmpeg -y -framerate 5 -i 'outputs/garden_brownplant_removal/07_final_visualization/merged/%05d.png' -vf "select='not(mod(n\,2))',scale=960:-1,setpts=N/TB/5" outputs/garden_brownplant_removal/animations/07_merged.gif
 
 echo "  - 07 comparison..."
-ffmpeg -y -framerate 10 -pattern_type glob -i 'outputs/garden_brownplant_removal/07_final_visualization/comparisons/*.png' -vf scale=1920:-1 outputs/garden_brownplant_removal/animations/07_comparison.gif
+ffmpeg -y -framerate 5 -i 'outputs/garden_brownplant_removal/07_final_visualization/comparisons/%05d.png' -vf "select='not(mod(n\,2))',scale=960:-1,setpts=N/TB/5" outputs/garden_brownplant_removal/animations/07_comparison.gif
 
 echo "  - 05c comparison..."
-ffmpeg -y -framerate 10 -pattern_type glob -i 'outputs/garden_brownplant_removal/05_scene_editing/05c_optimized/comparisons/*.png' -vf scale=1920:-1 outputs/garden_brownplant_removal/animations/05c_comparison.gif
+ffmpeg -y -framerate 5 -i 'outputs/garden_brownplant_removal/05_scene_editing/05c_optimized/comparisons/%05d.png' -vf "select='not(mod(n\,2))',scale=960:-1,setpts=N/TB/5" outputs/garden_brownplant_removal/animations/05c_comparison.gif
 
 # Kitchen Scene
 echo -e "\n[Kitchen] Creating animations..."
 mkdir -p outputs/kitchen_yellowtracker_removal/animations
 
 echo "  - 07 merged..."
-ffmpeg -y -framerate 10 -pattern_type glob -i 'outputs/kitchen_yellowtracker_removal/07_final_visualization/merged/*.png' -vf scale=1920:-1 outputs/kitchen_yellowtracker_removal/animations/07_merged.gif
+ffmpeg -y -framerate 5 -i 'outputs/kitchen_yellowtracker_removal/07_final_visualization/merged/%05d.png' -vf "select='not(mod(n\,2))',scale=960:-1,setpts=N/TB/5" outputs/kitchen_yellowtracker_removal/animations/07_merged.gif
 
 echo "  - 07 comparison..."
-ffmpeg -y -framerate 10 -pattern_type glob -i 'outputs/kitchen_yellowtracker_removal/07_final_visualization/comparisons/*.png' -vf scale=1920:-1 outputs/kitchen_yellowtracker_removal/animations/07_comparison.gif
+ffmpeg -y -framerate 5 -i 'outputs/kitchen_yellowtracker_removal/07_final_visualization/comparisons/%05d.png' -vf "select='not(mod(n\,2))',scale=960:-1,setpts=N/TB/5" outputs/kitchen_yellowtracker_removal/animations/07_comparison.gif
 
 echo "  - 05c comparison..."
-ffmpeg -y -framerate 10 -pattern_type glob -i 'outputs/kitchen_yellowtracker_removal/05_scene_editing/05c_optimized/comparisons/*.png' -vf scale=1920:-1 outputs/kitchen_yellowtracker_removal/animations/05c_comparison.gif
+ffmpeg -y -framerate 5 -i 'outputs/kitchen_yellowtracker_removal/05_scene_editing/05c_optimized/comparisons/%05d.png' -vf "select='not(mod(n\,2))',scale=960:-1,setpts=N/TB/5" outputs/kitchen_yellowtracker_removal/animations/05c_comparison.gif
 
 echo -e "\n=========================================="
 echo "✓ All animations created successfully!"
